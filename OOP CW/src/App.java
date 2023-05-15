cb9891a92213994fc6ff2fa95c43ba8e721a23c5
Lesnar Dsilva <lesnar.dsilva@gmail.com>
2023-05-15 11:13:07 +0100

    Delete OOP CW directory

--- OOP CW/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        WestminsterSkinConsultationManager x = new WestminsterSkinConsultationManager();
-        x.menu();
-    }
-}
-//ELAPSED TIME: 
-//Phase 1: 1hr - 2022/11/10
-//Phase 2: 1hr 44mins - 2022/11/10/
